0229047e3778dacef5dad95d40a02180655312c6
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-11-05 13:32:37 -0800

    connects server to mongodb

--- server/index.js
+++ server/index.js
@@ -3,9 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-
+//initializes the app
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+// connect to mongodb
+const CONNECTION_URL = "mongodb+srv://ghenikibrom:lovegheni0341@cluster0.p3uso.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
+.then(()=> app.listen(PORT, ()=> console.log(`Server is running so fast on port: ${PORT}`)))
+.catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify', false); //this prevents us from getting warnings in the console.sxcws
