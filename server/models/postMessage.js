271e3e7dd01986140d58741f3f882a0cf208a2d1
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-11-10 10:15:00 -0800

    create a mongoose Schema

---
+++ server/models/postMessage.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+//create a schema
+const postSchema = mongoose.Schema({
+title: String,
+message: String,
+creator: String,
+tags:[String],
+likeCount: {
+    type:Number,
+    default: 0
+},
+createedAt: {
+    type:Date,
+    default: new Date()
+},
+})
