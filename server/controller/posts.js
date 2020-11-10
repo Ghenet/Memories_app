af577af0f0c9cf6d426276ab91c70a8aacf5e6fc
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-11-10 10:34:51 -0800

    creates the createPost controller function and add it to the post route

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,5 +1,20 @@
 //here I'll create all the handlers(functions)  for our routes
 
+import PostMessage from "../models/postMessage.js";
+
 export const getPosts = (req, res) => {
     res.send('this Works');
+}
+
+//createPost  function
+export const createPost = async(req, res)=> {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
 }
