81d54e710b1b7cfb885b293a3d649f3676604731
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-11-12 14:58:14 -0800

    last update

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ import postRoutes from './routes/posts.js';
 //initializes the app
 const app = express();
 
-app.use('/posts', postRoutes); //middleware to connect the app with the routes
-
 app.use(bodyParser.json({ limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes); //middleware to connect the app with the routes
 
 // connect to mongodb
 const CONNECTION_URL = "mongodb+srv://ghenikibrom:lovegheni0341@cluster0.p3uso.mongodb.net/<dbname>?retryWrites=true&w=majority";
