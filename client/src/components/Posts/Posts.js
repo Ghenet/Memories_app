3c57b9603c8b59ef202a3aaf06c9599e96f66717
Ghenet Andemiachael <ghenikibrom@gmail.com>
2020-12-28 14:14:43 -0800

    maps through each post and displays them in grid

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import  { Grid, CircularProgress } from '@material-ui/core';
 
 import Post from './Post/Post';
 import useStyles from './styles';
@@ -10,11 +11,15 @@ const Posts = () => {
 
     console.log(posts);
     return (
-        <div>
-        <h1> Postsmm</h1>
-        <Post />
-        <Post />
-        </div>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stertch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
