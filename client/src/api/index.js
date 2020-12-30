d704bcdb79eb6d3e1e76ee94f5ebceb34a9e57c5
Ghenet Andemiachael <ghenikibrom@gmail.com>
2020-12-29 22:30:01 -0800

    api call for the update route

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,4 @@ const url = 'http://localhost:5000/posts';
 export const fetchPosts = () => axios.get(url);
 //api request for creating new post request
 export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
