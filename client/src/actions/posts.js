e662a3cd164acec65bc64ce6a5359bcf07ea0fe9
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-01-05 21:03:17 -0800

    adds the likePost action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -42,3 +42,14 @@ export const deletePost = (id) => async (dispatch) => {
         console.log(error);
     }
 }
+
+//likePost
+export const likePost = (id) => async(dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type:'UPDATE', payload:data });
+    } catch (error) {
+        console.log(error);
+    }
+}
