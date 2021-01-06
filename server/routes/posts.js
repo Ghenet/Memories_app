5fd39445ebb8a7033143f84a7608f38ba686f13e
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-01-05 20:45:10 -0800

    adds the route for likePost

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import express from 'express';
-import { getPosts, createPost, updatePost , deletePost } from '../controller/posts.js';
+import { getPosts, createPost, updatePost , deletePost, likePost } from '../controller/posts.js';
 
 const router = express.Router();
 
@@ -8,5 +8,5 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost); // updates an already existing post
 router.delete('/:id', deletePost);
-
+router.patch(':id/likePost', likePost)
 export default router;
